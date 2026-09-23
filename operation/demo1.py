ef7793027fcef41c29e6193dd63b78f8b46c5f1e
Sixuan Lin <Lin@gmail.com>
2026-09-23 14:26:46 +0800

    feat:"更新了demo1,创建了demo2"

--- operation/demo1.py
+++ operation/demo1.py
@@ -1,1 +1,23 @@
 print("hello world!")
+def cal(a,b,s):
+    if s == 'add':
+        return a+b
+    elif s == 'sub':
+        temp = a-b
+        if temp < 0:
+            return -temp
+        else:        
+            return temp
+    elif s == 'mul':
+        return a*b
+    else :
+        if a > b:
+            return a/b
+        else :
+            return b/a
+a = input()
+b = input()
+s = input()
+print(cal(a,b,s))
+
+            
